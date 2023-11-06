d835ec625bee0763a82e195d82a510e191f110d6
ClaireHowley <clairehowley2@gmail.com>
2023-11-06 21:40:42 +0100

    description table created

--- model/init_db.sql
+++ model/init_db.sql
@@ -14,17 +14,26 @@
 --
 
 DROP TABLE if exists questions;
-CREATE TABLE questions(
+CREATE TABLE questions (
     question_id INT NOT NULL AUTO_INCREMENT, 
     questions VARCHAR(40) not null, 
     PRIMARY KEY (question_id)
     );
 
 DROP TABLE if exists multipleChoiceAnswers;
-CREATE TABLE MultipleChoiceAnswers (
+CREATE TABLE multipleChoiceAnswers (
     answer_id INT PRIMARY KEY,
     answer_text TEXT,
     is_correct BOOLEAN,
     question_id INT,
-    FOREIGN KEY (question_id) REFERENCES Questions(question_id)
+    FOREIGN KEY (question_id) REFERENCES questions(question_id)
+);
+
+DROP TABLE if exists description;
+
+CREATE TABLE description (
+    description_id INT PRIMARY KEY,
+    description_title VARCHAR(255),
+    description_body TEXT,
+    image_url VARCHAR(255) 
 );
