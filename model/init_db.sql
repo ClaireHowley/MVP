25baa9bbe61c5178602ed2d021886df2291b6c40
ClaireHowley <clairehowley2@gmail.com>
2023-11-06 17:25:59 +0100

    mvp added in place of facebook in files

--- model/init_db.sql
+++ model/init_db.sql
@@ -3,16 +3,26 @@
 --
 
 SET foreign_key_checks = 0;
-DROP TABLE if exists students;
+DROP TABLE if exists questions;
 SET foreign_key_checks = 1;
+
+SET foreign_key_checks = 0;
+DROP TABLE if exists multipleChoiceAnswers;
+SET foreign_key_checks = 1;
+
 
 --
 -- Create Tables
 --
 
-CREATE TABLE students(
-    id INT NOT NULL AUTO_INCREMENT, 
-    firstname VARCHAR(40) not null, 
-    lastname VARCHAR(40) not null, 
+CREATE TABLE questions(
+    question_id INT NOT NULL AUTO_INCREMENT, 
+    questions VARCHAR(40) not null, 
+    PRIMARY KEY (id)
+    );
+
+CREATE TABLE multipleChoiceAnswers(
+    multipleChoiceAnswers_id INT NOT NULL AUTO_INCREMENT, 
+    questions VARCHAR(40) not null, 
     PRIMARY KEY (id)
     );
