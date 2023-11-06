063067491e65ac17c86cde7e9816e333e4eb1597
ClaireHowley <clairehowley2@gmail.com>
2023-11-06 17:59:50 +0100

    tables created manually

--- model/init_db.sql
+++ model/init_db.sql
@@ -2,27 +2,27 @@
 -- Drop Tables
 --
 
-SET foreign_key_checks = 0;
-DROP TABLE if exists questions;
-SET foreign_key_checks = 1;
+-- SET foreign_key_checks = 0;
+-- SET foreign_key_checks = 1;
 
-SET foreign_key_checks = 0;
-DROP TABLE if exists multipleChoiceAnswers;
-SET foreign_key_checks = 1;
+-- SET foreign_key_checks = 0;
+-- SET foreign_key_checks = 1;
 
 
 --
 -- Create Tables
 --
 
+DROP TABLE if exists questions;
 CREATE TABLE questions(
     question_id INT NOT NULL AUTO_INCREMENT, 
     questions VARCHAR(40) not null, 
-    PRIMARY KEY (id)
+    PRIMARY KEY (question_id)
     );
 
+DROP TABLE if exists multipleChoiceAnswers;
 CREATE TABLE multipleChoiceAnswers(
     multipleChoiceAnswers_id INT NOT NULL AUTO_INCREMENT, 
     questions VARCHAR(40) not null, 
-    PRIMARY KEY (id)
+    PRIMARY KEY (multipleChoiceAnswers_id)
     );
