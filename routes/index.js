d7da6a89a93508ea0f10a9b02252cf70f0eb6525
ClaireHowley <clairehowley2@gmail.com>
2023-11-07 12:18:01 +0100

    get one question added

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,27 @@ router.get("/", function (req, res, next) {
 	res.send({ title: "Express" });
 });
 
+/*GET question list */
+router.get("/questions", async function (req, res) {
+	try {
+		const results = await db("SELECT * FROM questions;");
+		res.send(results.data);
+	} catch (err) {
+		res.status(500).send(err);
+	}
+});
+
+// GET one question //
+const { id } = req.params;
+router.get("/questions/:id", async function (req, red) {
+	try {
+		const results = await db(
+			`"SELECT * FROM questions WHERE question_id = ${id};`
+		);
+		res.send(results.date[0]);
+	} catch (err) {
+		res.status(500).send(err);
+	}
+});
+
 module.exports = router;
