4f0c452fc136803439b5c32ed8312be69f6faa91
ClaireHowley <clairehowley2@gmail.com>
2023-11-08 16:41:02 +0100

    table added reviewpage

--- client/src/pages/ReviewPage.jsx
+++ client/src/pages/ReviewPage.jsx
@@ -1,15 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 export default function ReviewPage() {
 	return (
 		<div>
-			<button type="button" class="btn btn-primary">
-				<Link to="/">Homepage</Link>
-			</button>
-			<button type="button" class="btn btn-primary">
-				<Link to="/quiz">Start</Link>
-			</button>
+			<h1>Review</h1>
+			<h2>Present Tense - Ενεστώτας</h2>
+			<table border="5" className="table">
+				<tr>
+					<th></th>
+					<th>Group A</th>
+					<th>Group B1</th>
+					<th>Group B2</th>
+				</tr>
+				<tr>
+					<th>Εγώ</th>
+					<td>λύν-ω</td>
+					<td>αγαπ-ώ</td>
+					<td>μπορ-ώ</td>
+				</tr>
+				<tr>
+					<th>Εσύ</th>
+					<td>λύν-εις</td>
+					<td>αγαπ-άς</td>
+					<td>μπορ-είς</td>
+				</tr>
+				<tr>
+					<th> Αυτός/Αυτή/Αυτό </th>
+					<td>λύν-ει</td>
+					<td>αγαπ-ά(ει)</td>
+					<td>μπορ-εί</td>
+				</tr>
+				<tr>
+					<th> Εμείς </th>
+					<td>λύν-ουμε</td>
+					<td>αγαπ-άμε/ούμε</td>
+					<td>μπορ-ούμε</td>
+				</tr>
+				<tr>
+					<th> Εσείς </th>
+					<td>λύν-ετε</td>
+					<td>αγαπ-άτε</td>
+					<td>μπορ-είτε</td>
+				</tr>
+				<tr>
+					<th> Αυτοί/Αυτές/Αυτά </th>
+					<td>λύν-ουν(ε) </td>
+					<td>αγαπ-ούν/άνε</td>
+					<td>μπορ-ούν</td>
+				</tr>
+			</table>
+			<footer>
+				<button type="button" class="btn btn-primary">
+					<Link to="/">Homepage</Link>
+				</button>
+
+				<button type="button" class="btn btn-primary">
+					<Link to="/quiz">Quiz</Link>
+				</button>
+			</footer>
 		</div>
 	);
 }
